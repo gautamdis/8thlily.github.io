15d6e1ab3aa86656685343b3cf2ff6ed5dadffd0
gautamdis <108970454+gautamdis@users.noreply.github.com>
2022-07-30 12:20:22 +0530

    Website for 8th Lily

---
+++ time-table.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+include "header.php";
+?>
+<div id="table" >
+    <table>
+
+    </table>
+</div>
+</body>
+</html>
